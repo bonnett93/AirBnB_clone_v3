96581c2531772758266de5d0b9f33b2c7cb91ba0
dantereto <2247@holbertonschool.com>
2021-09-17 23:21:32 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,18 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    return jsonify({'amenities': storage.count('Amenity'),
+                    'cities': storage.count('City'),
+                    'places': storage.count('Place'),
+                    'reviews': storage.count('Review'),
+                    'states': storage.count('State'),
+                    'users': storage.count('User')})
