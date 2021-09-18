3ecc307bb20548eaf67fea53245ded3ffe88ef24
bonnett93 <bonnettjordan@gmail.com>
2021-09-17 21:30:11 -0500

    Modify index.py (clases), app.py status & pep8, __init__.py pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,12 +11,16 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(self):
+    """calls storage.close()"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """handler for 404 errors"""
     return (jsonify(error="Not found"), 404)
 
 if __name__ == "__main__":
