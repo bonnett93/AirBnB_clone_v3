ae14849d2bc0c9188a1062410c115f0a4d373c18
dantereto <2247@holbertonschool.com>
2021-09-18 00:36:45 +0000

    error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ import os
 from flask import Blueprint, render_template, abort
 from flask import Flask
 from models import storage
+from flask import jsonify
 from api.v1.views import app_views
 
 app = Flask(__name__)
@@ -13,6 +14,10 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return (jsonify(error="Not found"), 404)
 
 if __name__ == "__main__":
     make_h = os.getenv('HBNB_API_HOST', default='0.0.0.0')
