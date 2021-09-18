86a7a068b83ca21e132bbdbfd85533248137da1b
dantereto <2247@holbertonschool.com>
2021-09-18 00:39:01 +0000

    error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,8 +16,8 @@ def teardown(self):
     storage.close()
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return (jsonify(e="Not found"), 404)
+def page_not_found(error):
+    return (jsonify(error="Not found"), 404)
 
 if __name__ == "__main__":
     make_h = os.getenv('HBNB_API_HOST', default='0.0.0.0')
