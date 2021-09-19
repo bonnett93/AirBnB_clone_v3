235b336b27a2beb6adeef6b8a302b5c466c45a28
dantereto <2247@holbertonschool.com>
2021-09-19 15:49:05 +0000

    corse

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 
 import os
 from flask import Blueprint, render_template, abort
+from flask_cors import CORS
 from flask import Flask
 from models import storage
 from flask import jsonify
@@ -10,7 +11,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(self):
